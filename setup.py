cc2df3186c8f2d9fb5ba089e867cb4507009ea79
Kim Georg Lind Pedersen <georglind@gmail.com>
2016-09-26 16:09:43 +0200

    added source

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mollusk',
+      version='0.1',
+      description='Molecular Parser',
+      url='http://github.com/georglind/mol',
+      author='Kim G. L. Pedersen',
+      author_email='georglind@gmail.com',
+      license='MIT',
+      packages=['mol'],
+      zip_safe=False)
