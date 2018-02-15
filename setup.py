b070edae8c4f0b7a28ccf586764853c8543653af
Kim G L Pedersen <georglind@gmail.com>
2018-02-15 22:14:00 +0100

    Clean up.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 from setuptools.config import read_configuration
 
 cfg = read_configuration('./setup.cfg')
-#print(cfg)
 cfg["options"].update(cfg["metadata"])
 cfg=cfg["options"]
 setup(use_scm_version = True, **cfg)
